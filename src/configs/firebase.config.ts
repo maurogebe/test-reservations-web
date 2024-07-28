58f532b7fed6f491b7a1f4bb45330f35fe1d0388
Mauricio Guerra <maurogebe.96@gmail.com>
2024-07-27 23:24:32 -0500

    [feature][QP-24][Add Firebase config and CI CD]

---
+++ src/configs/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUdwcMS7RdioRZPMTVNhz0u1PhpwoVXEQ",
+  authDomain: "quickpharma-b8686.firebaseapp.com",
+  projectId: "quickpharma-b8686",
+  storageBucket: "quickpharma-b8686.appspot.com",
+  messagingSenderId: "385620055580",
+  appId: "1:385620055580:web:f25bb00ce7fc4b50c6f1d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
